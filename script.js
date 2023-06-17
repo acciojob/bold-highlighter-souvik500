e565246e2d225c0197cc37e2383653c20e844eab
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-06-17 13:07:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
     //Write your code here
-
-
+	var bold=document.getElementsByTagName("strong");
+	for(let i=0;i<bold.length;i++){
+		bold[i].classList.add("highlight");
+	}
 }
-
-
 function return_normal() {
     //Write your code here
-
+	var bold=document.getElementsByTagName("strong");
+     for(let i=0;i<bold.length;i++){
+		 bold[i].classList.remove("highlight");
+	 }
     
 }
